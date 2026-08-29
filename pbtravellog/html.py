b43ce8a03ba38e8e4fd22408de31ca07f36a25fb
Paul Bogard <paulbogard@gmail.com>
2026-08-29 09:56:24 -0400

    Create initial jinja2 template

--- pbtravellog/html.py
+++ pbtravellog/html.py
@@ -4,10 +4,14 @@
 import http.server
 import os
 import sys
+import shutil
 import webbrowser
 from functools import partial
+from importlib.resources import files, as_file
+from pathlib import Path
 
 # Third-party imports
+from jinja2 import Environment, PackageLoader
 
 HTML_PATH = os.getenv("PBTRAVELLOG_HTML_PATH")
 if HTML_PATH is None:
@@ -18,6 +22,23 @@ if HTML_PATH is None:
 def build():
     """Builds a directory of static HTML pages."""
     print("Building pbflightlog HTML...")
+    html_path = Path(HTML_PATH)
+
+    # Create root and static folders.
+    html_path.mkdir(parents=True, exist_ok=True)
+    static_dir = files("pbtravellog") / "static"
+    with as_file(static_dir) as static_path:
+        shutil.copytree(static_path, html_path, dirs_exist_ok=True)
+
+    env = Environment(
+        loader=PackageLoader("pbtravellog"),
+        autoescape=True,
+    )
+    site_index = env.get_template('site_index.html')
+    site_index_html = site_index.render()
+    (html_path / "index.html").write_text(site_index_html, encoding="utf-8")
+
+    print(f"Wrote static site to \"{html_path}\".")
 
 def run(port):
     """Launches a server and browser."""
@@ -36,7 +57,7 @@ def run(port):
         ("127.0.0.1", port), handler
     ) as httpd:
         url = f"http://localhost:{port}"
-        print(f"Serving {HTML_PATH} at {url}.")
+        print(f"Serving directory \"{HTML_PATH}\" at {url}.")
         webbrowser.open(url)
         try:
             httpd.serve_forever()
